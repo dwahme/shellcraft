36bdd77f77b474d696cf7aa8fd73bda4b6dcbd0c
Dawson Whitehead <dwahme1@gmail.com>
2020-02-15 10:47:49 -0600

    Added basic curses config

--- shellcraft/game.py
+++ shellcraft/game.py
@@ -1,8 +1,18 @@
+import curses
+
 
 class Game:
 
     def __init__(self):
-        pass
+        self.stdscr = None
 
+    # The main game loop, use run() instead
+    def __main(self, stdscr):
+        self.stdscr = stdscr
+
+        self.stdscr.nodelay(True)
+        self.stdscr.clear()
+
+    # Actually runs the game
     def run(self):
-        pass
+        curses.wrapper(self.__main)
