a8701ca86122963bc87fe1a6baa699ab3eb1f7fa
Dawson Whitehead <dwahme1@gmail.com>
2020-02-15 14:28:04 -0600

    Changed to support 3x5 blocks

--- shellcraft/game.py
+++ shellcraft/game.py
@@ -13,11 +13,11 @@ class Game:
         h, w = self.stdscr.getmaxyx()
         p_y, p_x = self.player.coords()
 
-        screen_start = (p_y * 3 - h // 2, p_x * 3 - w // 2)
+        screen_start = (p_y * 3 - h // 2, p_x * 5 - w // 2)
 
         for y in range(0, h - 2, 3):
-            for x in range(0, w - 2, 3):
-                ret = self.world.draw(y, x, (screen_start[0] + y) // 3, (screen_start[1] + x) // 3)
+            for x in range(0, w - 4, 5):
+                ret = self.world.draw(y, x, (screen_start[0] + y) // 3, (screen_start[1] + x) // 5)
 
     # The main game loop, use run() instead
     def __main(self, stdscr):
