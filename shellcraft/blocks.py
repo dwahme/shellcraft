771a84524a602a985dd37eac2baa2a682f0a59bd
yvesshum <yvesshum1210@gmail.com>
2020-02-15 14:34:28 -0600

    Added some documentation

--- shellcraft/blocks.py
+++ shellcraft/blocks.py
@@ -1,13 +1,13 @@
 import curses
+import sys
 from enum import Enum 
 from . pixel import Pixel
+
 
 class BType(): 
     """
     3 by 3 array of pixels
     """
-
-
     def __init__(self, blocktype):
         blocklist = {
             "STONE": [
@@ -32,7 +32,7 @@ class BType():
                     Pixel(" ", 16, 233, 12),
                     Pixel(" ", 16, 239, 13),
                     Pixel(" ", 16, 239, 14),
-                    Pixel(" ", 16, 247, 15),
+                    Pixel(" ", 16, 238, 15),
                 ]
             ],
             "DIRT": [[Pixel("X", curses.COLOR_GREEN, curses.COLOR_BLACK, 16)]],
@@ -40,36 +40,82 @@ class BType():
                 [
                     Pixel(" ", 16, 0, 31), 
                     Pixel(" ", 16, 240, 32),
-                    Pixel(" ", 16, 255, 33),
+                    Pixel(" ", 16, 249, 33),
                     Pixel(" ", 16, 247, 34),
                     Pixel(" ", 16, 0, 35),
 
                 ], 
                 [
                     Pixel(" ", 16, 0, 36),
-                    Pixel(" ", 16, 255, 37),
+                    Pixel(" ", 16, 249, 37),
                     Pixel(" ", 16, 246, 38),
-                    Pixel(" ", 16, 255, 39),
+                    Pixel(" ", 16, 251, 39),
                     Pixel(" ", 16, 0, 40),
 
                 ],
                 [
-                    Pixel(" ", 16, 255, 41),
-                    Pixel(" ", 16, 255, 42),
-                    Pixel(" ", 16, 255, 43),
-                    Pixel(" ", 16, 255, 44),
-                    Pixel(" ", 16, 255, 45),
+                    Pixel(" ", 16, 249, 41),
+                    Pixel(" ", 16, 250, 42),
+                    Pixel(" ", 16, 251, 43),
+                    Pixel(" ", 16, 250, 44),
+                    Pixel(" ", 16, 249, 45),
+                ]
+            ],
+            "WATER": [
+                [
+                    Pixel(" ", 16, 27, 46), 
+                    Pixel(" ", 16, 33, 47),
+                    Pixel(" ", 16, 27, 48),
+                    Pixel(" ", 16, 32, 49),
+                    Pixel(" ", 16, 27, 50),
+
+                ], 
+                [
+                    Pixel(" ", 16, 27, 51),
+                    Pixel(" ", 16, 27, 52),
+                    Pixel(" ", 16, 33, 53),
+                    Pixel(" ", 16, 27, 54),
+                    Pixel(" ", 16, 32, 55),
+
+                ],
+                [
+                    Pixel(" ", 16, 33, 56),
+                    Pixel(" ", 16, 27, 57),
+                    Pixel(" ", 16, 32, 58),
+                    Pixel(" ", 16, 33, 59),
+                    Pixel(" ", 16, 26, 60),
+                ]
+            ],
+            "COMP": [
+                [
+                    Pixel("#", 16, 254, 76), 
+                    Pixel(" ", 16, 16, 77),
+                    Pixel(" ", 16, 16, 78),
+                    Pixel(" ", 16, 254, 79),
+                    Pixel("#", 16, 254, 80),
+
+                ], 
+                [
+                    Pixel(" ", 16, 254, 81),
+                    Pixel(" ", 16, 254, 82),
+                    Pixel(" ", 16, 254, 83),
+                    Pixel(" ", 16, 16, 84),
+                    Pixel(" ", 16, 16, 85),
+
+                ],
+                [
+                    Pixel("#", 16, 254, 86),
+                    Pixel(" ", 16, 16, 87),
+                    Pixel(" ", 16, 16, 88),
+                    Pixel(" ", 16, 254, 89),
+                    Pixel("#", 16, 254, 90),
                 ]
             ]
         }
         self.blocks = blocklist[blocktype]
 
 
-    # CLOUD = [[]]
-    # DIRT = [[]]
-    # DIRTGRASS = [[]]
-    # WATER = [[]]
-    # SAND = [[]]
+    # SAND = [[]] 91-105
     # COMP = [[]]
     # WIRE = [[]]
     # AIR = [[]]
@@ -78,6 +124,10 @@ class BType():
 class Block:
     """
     This class generates a 3x3 block, given a type
+
+    example:
+    b = Block("STONE", self.stdscr)
+
     """
     def __init__(self, blocktype, screen): 
         self.blocktype = BType(blocktype)
@@ -86,7 +136,8 @@ class Block:
     def draw(self, locY, locX):
         """
         This function draws out a particular block given screen location
-        * y+1
+        
+        e.g. b.draw(10, 5)
         """
         # print('=========' )
         # curses.init_pair(9, curses.COLOR_RED, curses.COLOR_WHITE)
